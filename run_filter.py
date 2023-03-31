634efe3279ab584ddcdf73e0f897da59e0ffe105
suksien <hahasuksien@yahoo.com.my>
2023-03-31 09:38:52 -0700

    adding final dataset

--- run_filter.py
+++ run_filter.py
@@ -6,5 +6,5 @@ df = pd.read_excel(filename)
 
 colon_idx, _ = utils.filter_results(df)
 sub_df = df.iloc[colon_idx]
-sub_df.to_excel('master_output_colon2.xlsx')
+sub_df.to_excel('master_output_colon_noerratum.xlsx')
 
